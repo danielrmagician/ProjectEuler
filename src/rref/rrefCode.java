c968cc4b6caa671ff613cdb60ff316a2c3fae599
dkrui01 <dkrui01@P2P61824A.admin.bvsd.k12.co.us>
2017-10-10 12:48:04 -0600

    create matrix class

--- src/rref/rrefCode.java
+++ src/rref/rrefCode.java
@@ -1,5 +1,47 @@
 package rref;
 
 public class rrefCode {
+	
+	public static double[][] rref(double[][] mat)
+	{
+	    double[][] rref = new double[mat.length][mat[0].length];
 
+	    /* Copy matrix */
+	    for (int r = 0; r < rref.length; ++r)
+	    {
+	        for (int c = 0; c < rref[r].length; ++c)
+	        {
+	            rref[r][c] = mat[r][c];
+	        }
+	    }
+
+	    for (int p = 0; p < rref.length; ++p)
+	    {
+	        /* Make this pivot 1 */
+	        double pv = rref[p][p];
+	        if (pv != 0)
+	        {
+	            double pvInv = 1.0 / pv;
+	            for (int i = 0; i < rref[p].length; ++i)
+	            {
+	                rref[p][i] *= pvInv;
+	            }
+	        }
+
+	        /* Make other rows zero */
+	        for (int r = 0; r < rref.length; ++r)
+	        {
+	            if (r != p)
+	            {
+	                double f = rref[r][p];
+	                for (int i = 0; i < rref[r].length; ++i)
+	                {
+	                    rref[r][i] -= f * rref[p][i];
+	                }
+	            }
+	        }
+	    }
+
+	    return rref;
+	}
 }
---
+++ src/rref/rrefMatrixRunner.java
@@ -0,0 +1,10 @@
+package rref;
+
+public class rrefMatrixRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
