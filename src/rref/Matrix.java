bfcfc402dd2f6bf52d3ba2987d56a93a4b4b4438
dkrui01 <dkrui01@P2P61824A.admin.bvsd.k12.co.us>
2017-10-30 12:41:11 -0600

    MEH

--- src/rref/Matrix.java
+++ src/rref/Matrix.java
@@ -11,16 +11,23 @@ public class Matrix {
 	public void rref() {
 		for (int i = 0; i<matrix[0].length-1; i++) {
 			for (int j = 0; j<matrix.length; j++) {
-				Fraction multiple = matrix[i][i].divide(matrix[i][j]);
-				if (i!=j) {
+				Fraction multiple = matrix[i][i].divide(matrix[j][i]);
+				if (i<j) {
 					multrowadd(multiple, i, j);
+				} else if (i>j) {
+					multrowaddneg(multiple, i, j);
 				}
 			}
 		}
 	}
 	private void multrowadd(Fraction multiple, int row1, int row2) {
 		for (int col = 0; col<matrix[0].length; col++) {
-			matrix[row1][col]=matrix[row1][col].subtract(matrix[row2][col].multiply(multiple));
+			matrix[row2][col]=matrix[row1][col].subtract(matrix[row2][col].multiply(multiple));
+		}
+	}
+	private void multrowaddneg(Fraction multiple, int row1, int row2) {
+		for (int col = 0; col<matrix[0].length; col++) {
+			matrix[row1][col]=(matrix[row2][col].multiply(multiple)).subtract(matrix[row1][col]);
 		}
 	}
 	public String toString() {
--- src/rref/MatrixRunner.java
+++ src/rref/MatrixRunner.java
@@ -4,7 +4,36 @@ public class MatrixRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n = 3;
+		Fraction[][] matrix = new Fraction[n][n+1];
+		for (int i = 1; i<=n; i++) {
+			for (int j = 1; j<=n+1; j++) {
+				if (j!=n+1) {
+					Fraction frac = new Fraction((int)Math.pow(i, j),1);
+					matrix[i-1][j-1] = frac;
+				}
+				else {
+					matrix[i-1][j-1] = new Fraction(i*i+1,1);
+				}
+			}
+		}
+		Matrix matrx = new Matrix(matrix);
+		matrx.rref();
+// 		Print out matrix
+		System.out.println(matrx.toString());
+		
+//		Print out last column of rrefed matrix
+//		for (int i = 0; i < matrix.length; i++) {
+//		    System.out.print(matrix[i][matrix.length] + " ");
+//		    System.out.println();
+//		}
+//		Print out f(100)
+//		double hundred=0;
+//		for (int i = 0; i < matrix.length; i++) {
+//			double newTerm= matrix[i][matrix.length]*Math.pow(14, i);
+//			hundred = hundred + newTerm;
+//		}
+//		System.out.println("The value of the function at a hundred is " + hundred);
 	}
 
 }
